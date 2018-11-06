72931481d375635a2cab6b8dff1cdaec0ce34b9a
Mikhail Belov <mpbelov@gmail.com>
2018-11-06 23:17:06 +0300

    Candy amount - fixed algorithm for rating with descending sort

--- DP/CandiesAmount/BruteForceSolution.cs
+++ DP/CandiesAmount/BruteForceSolution.cs
@@ -2,32 +2,39 @@ using System;
 
 namespace CandiesAmount {
     public static class BruteForceSolution {
-        public static long Candies(params int[] rating) {
+        public static long Candies(params int[] arr) {
+            var rating = arr;
             var candies = new int[rating.Length];
-            for (int i = 0; i < candies.Length; i++) {
-                candies[i] = 1;
-            }
-            bool wasUpdate;
-            do {
-                wasUpdate = false;
-                for (int i = 1; i < rating.Length; i++) {
-                    if (rating[i] > rating[i - 1] && candies[i] <= candies[i - 1]) {
-                        candies[i] = candies[i - 1] + 1;
-                        wasUpdate = true;
-                    }
-                    else if (rating[i] < rating[i - 1] && candies[i] >= candies[i - 1]) {
-                        candies[i - 1] = candies[i] + 1;
-                        wasUpdate = true;
-                    }
+            // we should specify 1 as a default value,
+            // otherwise all candies amount to be incremented by 1 in the end
+            // for (int i = 0; i < candies.Length; i++) {
+            //     candies[i] = 1;
+            // }
+            
+            // go forward and find candy amount for elements with ascending rating sort
+            for (int i = 1; i < rating.Length; i++) {
+                if (rating[i] > rating[i - 1] && candies[i] <= candies[i - 1]) {
+                    candies[i] = candies[i - 1] + 1;
                 }
-            } while (wasUpdate);
-
-            var sum = 0;
-            for (int i = 0; i < candies.Length; i++) {
-                Console.Write(candies[i] + " ");
-                sum += candies[i];
+                else if (rating[i] < rating[i - 1] && candies[i] >= candies[i - 1]) {
+                    candies[i - 1] = candies[i] + 1;
+                }
             }
-            Console.WriteLine();
+            // go backward and find candy amount for elements with descending rating sort
+            for (int i = rating.Length - 1; i > 0; i--) {
+                if (rating[i] > rating[i - 1] && candies[i] <= candies[i - 1]) {
+                    candies[i] = candies[i - 1] + 1;
+                }
+                else if (rating[i] < rating[i - 1] && candies[i] >= candies[i - 1]) {
+                    candies[i - 1] = candies[i] + 1;
+                }
+            }
+
+            long sum = 0;
+            for (int i = 0; i < candies.Length; i++) {
+                // just add 1 to each candy amount to get correct result
+                sum += candies[i] + 1;
+            }
             return sum;
         }
     }
--- DP/CandiesAmount/Program.cs
+++ DP/CandiesAmount/Program.cs
@@ -4,8 +4,9 @@ using ExecutionTimeTracker;
 namespace CandiesAmount {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(TimeTracker.Execute(() => Console.WriteLine(MemoizationSolution.Candies(1, 2, 2))).TotalSeconds); // 4
-            Console.WriteLine(TimeTracker.Execute(() => Console.WriteLine(MemoizationSolution.Candies(2, 4, 2, 6, 1, 7, 8, 9, 2, 1))).TotalSeconds); // 19
+            Console.WriteLine(TimeTracker.Execute(() => Console.WriteLine(BruteForceSolution.Candies(1, 2, 2))).TotalSeconds); // 4
+            Console.WriteLine(TimeTracker.Execute(() => Console.WriteLine(BruteForceSolution.Candies(2, 4, 2, 6, 1, 7, 8, 9, 2, 1))).TotalSeconds); // 19
+            Console.WriteLine(TimeTracker.Execute(() => Console.WriteLine(BruteForceSolution.Candies(10, 9, 8, 7, 6, 5, 4, 3, 2, 1))).TotalSeconds); // 55
         }
     }
 }
