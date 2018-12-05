26a8a0fb2b7fa116053a163311eaf6f29936469f
Mikhail Belov <mpbelov@gmail.com>
2018-12-05 21:09:22 +0300

    Abbreviation: added memoization

--- DP/Abbreviation/RecursionSolution.cs
+++ DP/Abbreviation/RecursionSolution.cs
@@ -2,17 +2,23 @@ using System.Text;
 namespace Abbreviation {
     public static class RecursionSolution {
         public static string abbreviation(string a, string b) {
-            var result = abbreviation1(a, b);
+            var result = abbreviation1(a, b, new bool?[a.Length + 1, b.Length + 1]);
             return result ? "YES" : "NO";
         }
 
-        private static bool abbreviation1(string a, string b) {
+        private static bool abbreviation1(string a, string b, bool?[,] dp) {
             if (a.Length == 0 && b.Length == 0) {
                 return true;
             }
             if (a.Length == 0 && b.Length > 0) {
                 return false;
             }
+
+            var r = dp[a.Length, b.Length];
+            if (r.HasValue) {
+                return r.Value;
+            }
+
             if (a.Length > 0 && b.Length == 0) {
                 for (int i = 0; i < a.Length; i++) {
                     if (char.IsUpper(a[i])) {
@@ -27,20 +33,26 @@ namespace Abbreviation {
 
             if (char.IsUpper(ca)) {
                 if (ca == cb) {
-                    return abbreviation1(a.Remove(0, 1), b.Remove(0, 1));
+                    var result = abbreviation1(a.Remove(0, 1), b.Remove(0, 1), dp);
+                    dp[a.Length, b.Length] = result;
+                    return result;
                 }
                 else {
+                    dp[a.Length, b.Length] = false;
                     return false;
                 }
             }
             else { // a is lower
                 if (char.ToUpper(ca) == cb) {
-                    var res1 = abbreviation1(a.Remove(0, 1), b.Remove(0, 1));
-                    var res2 = abbreviation1(a.Remove(0, 1), b);
+                    var res1 = abbreviation1(a.Remove(0, 1), b.Remove(0, 1), dp);
+                    var res2 = abbreviation1(a.Remove(0, 1), b, dp);
+                    dp[a.Length, b.Length] = res1 | res2;
                     return res1 | res2;
                 }
                 else {
-                    return abbreviation1(a.Remove(0, 1), b);
+                    var result = abbreviation1(a.Remove(0, 1), b, dp);
+                    dp[a.Length, b.Length] = result;
+                    return result;
                 }
             }
         }
