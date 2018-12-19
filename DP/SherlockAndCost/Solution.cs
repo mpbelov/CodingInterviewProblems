0bc916e0546b33c753ec491a9a5143840047756b
Mikhail Belov <mpbelov@gmail.com>
2018-12-19 10:42:34 +0300

    Sherlock and Cost: correct solution

--- DP/SherlockAndCost/Solution.cs
+++ DP/SherlockAndCost/Solution.cs
@@ -2,24 +2,25 @@ using System;
 
 namespace SherlockAndCost {
     static class Solution {
+        // We should calculate 2 sums and compare them. The max one is the answer.
+        // The sum would be max if the difference between two consecutive numbers if one of them is 1
+        // The first one could be 1, or the second one. After that we should calculate 2 sums and compare them.
         public static int cost(int[] B) {
-            // We should calculate 2 sums and compare them. The max one is the answer.
-            // The sum would be max if the difference between two consecutive numbers if one of them is 1
-            // The first one could be 1, or the second one. After that we should calculate 2 sums and compare them.
-            var way1 = 0;
-            var way2 = 0;
+            var firstIs1Way = 0;
+            var secondIs1Way = 0;
 
             for (int i = 1; i < B.Length; i++) {
-                var fisrtIs1 = Math.Abs(B[i] - 1);
+                var firstIs1 = Math.Abs(B[i] - 1);
                 var secondIs1 = Math.Abs(1 - B[i - 1]);
 
-                var way1New = Math.Max(way2, way1 + secondIs1);
-                var way2New = Math.Max(way1, way2 + fisrtIs1);
+                var firstIs1WayTemp = Math.Max(firstIs1Way, secondIs1Way + firstIs1);
+                var secondIs1WayTemp = Math.Max(secondIs1Way, firstIs1Way + secondIs1);
 
-                way1 = way1New;
-                way2 = way2New;
+                firstIs1Way = firstIs1WayTemp;
+                secondIs1Way = secondIs1WayTemp;
             }
-            return Math.Max(way1, way2);
+            
+            return Math.Max(firstIs1Way, secondIs1Way);
         }
     }
 }
