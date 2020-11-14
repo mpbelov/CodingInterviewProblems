9ce7abc6f57f6eb0324270aecc399ceb19fbb81e
Mikhail Belov <mpbelov@gmail.com>
2020-11-14 16:16:44 +0000

    BiweeklyContest-39

---
+++ Leetcode/BiweeklyContest-39/BalancedString/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BalancedString {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(MinimumDeletions("aababbab"));
+        }
+
+        // https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/
+        public static int MinimumDeletions(string s) {
+            if (s.Length == 1) {
+                return 0;
+            }
+
+            var bsLeft = new int[s.Length]; // number of 'b's to the left of the current character
+            var asRight = new int[s.Length]; // number of 'a's to the right of the current character
+
+            bsLeft[0] = 0; // no 'b's to the left
+            asRight[asRight.Length - 1] = 0; // no 'a's to the rigth
+
+            for (int i = 1; i < s.Length; i++) {
+                if (s[i - 1] == 'b') {
+                    bsLeft[i] = bsLeft[i - 1] + 1;
+                }
+                else {
+                    bsLeft[i] = bsLeft[i - 1];
+                }
+            }
+
+            for (int i = s.Length - 2; i >= 0; i--) {
+                if (s[i + 1] == 'a') {
+                    asRight[i] = asRight[i + 1] + 1;
+                }
+                else {
+                    asRight[i] = asRight[i + 1];
+                }
+            }
+
+            var minDeletions = int.MaxValue;
+            for (int i = 0; i < bsLeft.Length; i++) {
+                var curDeletions = bsLeft[i] + asRight[i];
+                minDeletions = Math.Min(minDeletions, curDeletions);
+            }
+
+            return minDeletions;
+        }
+    }
+}
---
+++ Leetcode/BiweeklyContest-39/DefuseTheBomb/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DefuseTheBomb {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(string.Join(",", Decrypt(new int[] { 5, 7, 1, 4 }, 3)));
+        }
+
+        // https://leetcode.com/problems/defuse-the-bomb/
+        static int[] Decrypt(int[] code, int k) {
+            var newCode = new int[code.Length];
+
+            if (k > 0) {
+                for (int i = 0; i < newCode.Length; i++) {
+                    int sum = 0;
+                    int nextIndex = i + 1;
+                    int left = k;
+                    while (left-- > 0) {
+                        if (nextIndex == code.Length) {
+                            nextIndex = 0;
+                        }
+                        sum += code[nextIndex++];
+                    }
+                    newCode[i] = sum;
+                }
+            }
+            else if (k < 0) {
+                for (int i = 0; i < newCode.Length; i++) {
+                    int sum = 0;
+                    int nextIndex = i - 1;
+                    int left = k;
+                    while (left++ < 0) {
+                        if (nextIndex == -1) {
+                            nextIndex = code.Length - 1;
+                        }
+                        sum += code[nextIndex--];
+                    }
+                    newCode[i] = sum;
+                }
+            }
+            else {
+                for (int i = 0; i < newCode.Length; i++) {
+                    newCode[i] = 0;
+                }
+            }
+            return newCode;
+        }
+    }
+}
